f99d3fea18cf16909863524ea4c43a2541131c0d
Vazir Vadirov <ggvalion@gmail.com>
2025-04-07 17:23:20 +0500

    Added command to set world spawn in another dimension

--- src/main/kotlin/net/galiev/sws/commands/WorldsCommands.kt
+++ src/main/kotlin/net/galiev/sws/commands/WorldsCommands.kt
@@ -2,13 +2,22 @@ package net.galiev.sws.commands
 
 import com.mojang.brigadier.CommandDispatcher
 import com.mojang.brigadier.context.CommandContext
+import net.galiev.sws.config.Config
+import net.galiev.sws.config.ConfigManager
+import net.galiev.sws.config.ExactSpawn
+import net.galiev.sws.config.RangeSpawn
 import net.galiev.sws.helper.WorldHelper.dims
+import net.minecraft.command.argument.AngleArgumentType
+import net.minecraft.command.argument.BlockPosArgumentType
+import net.minecraft.command.argument.DimensionArgumentType
 import net.minecraft.server.command.CommandManager
 import net.minecraft.server.command.ServerCommandSource
+import net.minecraft.server.world.ServerWorld
 import net.minecraft.text.ClickEvent
 import net.minecraft.text.HoverEvent
 import net.minecraft.text.Text
 import net.minecraft.util.Formatting
+import net.minecraft.util.math.BlockPos
 
 object WorldsCommands {
     fun register(dispatcher: CommandDispatcher<ServerCommandSource>){
@@ -16,6 +25,50 @@ object WorldsCommands {
             CommandManager.literal("sws")
                 .then(CommandManager.literal("worlds").executes { getAllWorlds(it) })
                 .then(CommandManager.literal("playerWorld").executes { getPlayerWorld(it) })
+                .then(CommandManager.literal("setspawnworld")
+                    .requires { it.hasPermissionLevel(2) }
+                    .executes {
+                        setSpawnWorldSpawn(
+                            it,
+                            it.source.world,
+                            BlockPos.ofFloored(it.source.position),
+                            0.0F
+                        )
+                    }
+                    .then(
+                        CommandManager.argument("dimension", DimensionArgumentType.dimension())
+                            .executes {
+                                setSpawnWorldSpawn(
+                                    it,
+                                    DimensionArgumentType.getDimensionArgument(it, "dimension"),
+                                    BlockPos.ofFloored(it.source.position),
+                                    0.0F
+                                )
+                            }
+                            .then(
+                                CommandManager.argument("pos", BlockPosArgumentType.blockPos())
+                                    .executes {
+                                        setSpawnWorldSpawn(
+                                            it,
+                                            DimensionArgumentType.getDimensionArgument(it, "dimension"),
+                                            BlockPosArgumentType.getValidBlockPos(it, "pos"),
+                                            0.0F
+                                        )
+                                    }
+                            )
+                            .then(
+                                CommandManager.argument("angle", AngleArgumentType.angle())
+                                    .executes {
+                                        setSpawnWorldSpawn(
+                                            it,
+                                            DimensionArgumentType.getDimensionArgument(it, "dimension"),
+                                            BlockPosArgumentType.getValidBlockPos(it, "pos"),
+                                            AngleArgumentType.getAngle(it, "angle")
+                                        )
+                                    }
+                            )
+                    )
+                )
         )
     }
 
@@ -37,5 +90,28 @@ object WorldsCommands {
                 it.withHoverEvent(HoverEvent.ShowText(Text.literal("Copy To Clipboard")))
                     .withClickEvent(ClickEvent.CopyToClipboard(playerWorld.toString())) }}, false)
         return 1
+    }
+
+    private fun setSpawnWorldSpawn(context: CommandContext<ServerCommandSource>, serverWorld: ServerWorld, pos: BlockPos, angle: Float): Int {
+        if (dims.contains(serverWorld.registryKey.value)) {
+            serverWorld.setSpawnPos(pos, angle)
+            ConfigManager.write(
+                Config(
+                    serverWorld.registryKey.value.toString(),
+                    safeCheck = false,
+                    isRangeSpawn = false,
+                    isExactSpawn = true,
+                    RangeSpawn(0, 0),
+                    ExactSpawn(pos.x, pos.y, pos.z)
+                )
+            )
+            context.source.sendFeedback ({
+                Text.translatable("commands.setworldspawn.success", serverWorld.registryKey.value.toString(), pos.x, pos.y, pos.z, angle)
+            }, false)
+            return 1
+        } else {
+            context.source.sendError(Text.literal("World not exist"))
+            return 0
+        }
     }
 }
